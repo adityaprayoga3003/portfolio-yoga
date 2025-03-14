fb1b29b085ba6e837a6a49c5d6eac509e214b61b
adityaprayoga3003 <127071545+adityaprayoga3003@users.noreply.github.com>
2025-03-15 05:55:55 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -6,49 +6,74 @@ menuIcon.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if (top > offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-            });
+    if (top > offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
 
-            document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
-        }
-    });
+      document
+        .querySelector('header nav a[href*="' + id + '"]')
+        .classList.add("active");
+    }
+  });
 
-    // Sticky navbar
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-    menuIcon.classList.remove('fa-xmark');
-    navbar.classList.remove('active');
+  // Sticky navbar
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+  menuIcon.classList.remove("fa-xmark");
+  navbar.classList.remove("active");
 };
 
 // Inisialisasi ScrollReveal di luar fungsi onscroll
 ScrollReveal({
-    distance: '80px',
-    duration: 2000,
-    delay: 200,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 // Inisialisasi Typed.js di luar fungsi onscroll
-const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Web Designer', 'Youtuber'],
-    typeSpeed: 70,
-    backSpeed: 70,
-    backDelay: 1000,
-    loop: true
+const typed = new Typed(".multiple-text", {
+  strings: [
+    "System Analyst",
+    "Web Development",
+    "UI/UX Designer",
+    "Project Manager",
+    "YouTuber",
+  ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+});
+
+const typed1 = new Typed(".multiple-text1", {
+  strings: [
+    "System Analyst",
+    "Web Development",
+    "UI/UX Designer",
+    "Project Manager",
+    "YouTuber",
+  ],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
 });
